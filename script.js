7a73c8616fc8b291725a8030b7e0e46696656a02
Gexof <mohamed.ashraf.abdelmounaim.gmail.com>
2024-01-13 01:32:26 +0200

    implment update func

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let courtDesc = document.getElementById("court-desc");
 let courtType = document.getElementById("court-type");
 let courtPrice = document.getElementById("court-price");
 
+let updateCourtBtn = document.getElementById("update-court");
+updateCourtBtn.style.display = "none";
+
 let allCourts = [];
 
 if (localStorage.getItem("Courts") != null) {
@@ -20,7 +23,7 @@ function showCourtData() {
     <td>${allCourts[i].courtType}</td>
     <td>${allCourts[i].courtPrice}</td>
     <td>
-    <button onclick="" class="edit-btn"><i class="fa-regular fa-pen-to-square"></i></button></td>
+    <button onclick="editCourt(${i})" class="edit-btn"><i class="fa-regular fa-pen-to-square"></i></button></td>
     <td><button onclick="deleteRow(${i})" class="del-btn"><i class="fa-solid fa-trash-can"></i></button></td>
   </tr>
     `;
@@ -39,7 +42,6 @@ function addNewCourt() {
   localStorage.setItem("Courts", JSON.stringify(allCourts));
   showCourtData();
 
-  courtId.value = "";
   courtDesc.value = "";
   courtPrice.value = "";
 }
@@ -49,9 +51,42 @@ addCourtBtn.addEventListener("click", addNewCourt);
 
 showCourtData();
 
-// delete row
 function deleteRow(i) {
   allCourts.splice(i, 1);
   localStorage.Courts = JSON.stringify(allCourts);
   showCourtData();
+}
+
+function editCourt(i) {
+  addCourtBtn.style.display = "none";
+  updateCourtBtn.style.display = "block";
+
+  let selectedCourt = allCourts[i];
+
+  courtDesc.value = selectedCourt.courtDesc;
+  courtType.value = selectedCourt.courtType;
+  courtPrice.value = selectedCourt.courtPrice;
+
+  // updateCourtBtn.removeEventListener("click", updateHandler);
+
+  function updateHandler() {
+    selectedCourt.courtDesc = courtDesc.value;
+    selectedCourt.courtType = courtType.value;
+    selectedCourt.courtPrice = courtPrice.value;
+
+    localStorage.setItem("Courts", JSON.stringify(allCourts));
+
+    showCourtData();
+
+    courtDesc.value = "";
+    courtType.value = "";
+    courtPrice.value = "";
+
+    addCourtBtn.style.display = "block";
+    updateCourtBtn.style.display = "none";
+
+    updateCourtBtn.removeEventListener("click", updateHandler);
+  }
+
+  updateCourtBtn.addEventListener("click", updateHandler);
 }
